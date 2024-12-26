symfony/demo

Remove an unneeded state in the SourceCodeExtension

src/AppBundle/Twig/SourceCodeExtension.php
@@ -24,7 +24,6 @@ class SourceCodeExtension extends \Twig_Extension
 {
     protected $loader;
     protected $controller;
-    protected $template;
     protected $kernelRootDir;
 
     public function __construct(\Twig_LoaderInterface $loader, $kernelRootDir)
@@ -48,13 +47,11 @@ class SourceCodeExtension extends \Twig_Extension
         );
     }
 
-    public function showSourceCode(\Twig_Environment $twig, $template)
+    public function showSourceCode(\Twig_Environment $twig, \Twig_Template $template)
     {
-        $this->template = $template;
-
         return $twig->render('default/_source_code.html.twig', array(
             'controller' => $this->getController(),
-            'template'   => $this->getTemplate(),
+            'template'   => $this->getTemplate($template),
         ));
     }
 
@@ -80,9 +77,9 @@ class SourceCodeExtension extends \Twig_Extension
         );
     }
 
-    private function getTemplate()
+    private function getTemplate(\Twig_Template $template)
     {
-        $templateName = $this->template->getTemplateName();
+        $templateName = $template->getTemplateName();
 
         return array(
             'file_path' => $this->kernelRootDir.'/Resources/views/'.$templateName,

muqeetsyed/symfony-webencore

src/AppBundle/Twig/SourceCodeExtension.php
@@ -24,7 +24,6 @@ class SourceCodeExtension extends \Twig_Extension
 {
     protected $loader;
     protected $controller;
-    protected $template;
     protected $kernelRootDir;
 
     public function __construct(\Twig_LoaderInterface $loader, $kernelRootDir)
@@ -48,13 +47,11 @@ class SourceCodeExtension extends \Twig_Extension
         );
     }
 
-    public function showSourceCode(\Twig_Environment $twig, $template)
+    public function showSourceCode(\Twig_Environment $twig, \Twig_Template $template)
     {
-        $this->template = $template;
-
         return $twig->render('default/_source_code.html.twig', array(
             'controller' => $this->getController(),
-            'template'   => $this->getTemplate(),
+            'template'   => $this->getTemplateSource($template),
         ));
     }
 
@@ -80,9 +77,9 @@ class SourceCodeExtension extends \Twig_Extension
         );
     }
 
-    private function getTemplate()
+    private function getTemplateSource(\Twig_Template $template)
     {
-        $templateName = $this->template->getTemplateName();
+        $templateName = $template->getTemplateName();
 
         return array(
             'file_path' => $this->kernelRootDir.'/Resources/views/'.$templateName,