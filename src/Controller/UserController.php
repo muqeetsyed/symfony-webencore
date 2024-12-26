symfony/demo

Add ability to edit user's informations and password

src/Controller/UserController.php
@@ -1,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Form\Type\ChangePasswordType;
+use App\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Controller used to manage current user.
+ *
+ * @Route("/user")
+ * @Security("has_role('ROLE_USER')")
+ *
+ * @author Romain Monteil <monteil.romain@gmail.com>
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET", "POST"}, name="user_index")
+     */
+    public function index(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'user.updated_successfully');
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/password", methods={"GET", "POST"}, name="user_password")
+     */
+    public function password(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($encoder->encodePassword($user, $form->get('newPassword')->getData()));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('security_logout');
+        }
+
+        return $this->render('user/password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}

tests/Controller/UserControllerTest.php
@@ -1,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Functional test for the controllers defined inside the UserController used
+ * for managing the current logged user.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#functional-tests
+ *
+ * Whenever you test resources protected by a firewall, consider using the
+ * technique explained in:
+ * https://symfony.com/doc/current/cookbook/testing/http_authentication.html
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class UserControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider getUrlsForAnonymousUsers
+     */
+    public function testAccessDeniedForAnonymousUsers(string $httpMethod, string $url)
+    {
+        $client = static::createClient();
+        $client->request($httpMethod, $url);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            'http://localhost/en/login',
+            $response->getTargetUrl(),
+            sprintf('The %s secure URL redirects to the login form.', $url)
+        );
+    }
+
+    public function getUrlsForAnonymousUsers()
+    {
+        yield ['GET', '/en/user/'];
+        yield ['GET', '/en/user/password'];
+    }
+
+    public function testEditUser()
+    {
+        $newUserEmail = 'admin_jane@symfony.com';
+
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+        $crawler = $client->request('GET', '/en/user/');
+        $form = $crawler->selectButton('Save changes')->form([
+            'user[email]' => $newUserEmail,
+        ]);
+        $client->submit($form);
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        /** @var User $user */
+        $user = $client->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy([
+            'email' => $newUserEmail,
+        ]);
+        $this->assertNotNull($user);
+        $this->assertSame($newUserEmail, $user->getEmail());
+    }
+
+    public function testChangePassword()
+    {
+        $newUserPassword = 'new-password';
+
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+        $crawler = $client->request('GET', '/en/user/password');
+        $form = $crawler->selectButton('Save changes')->form([
+            'change_password[currentPassword]' => 'kitten',
+            'change_password[newPassword][first]' => $newUserPassword,
+            'change_password[newPassword][second]' => $newUserPassword,
+        ]);
+        $client->submit($form);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            '/en/logout',
+            $response->getTargetUrl(),
+            'Changing password logout the user.'
+        );
+    }
+}

muqeetsyed/symfony-webencore

src/Controller/UserController.php
@@ -1,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Form\Type\ChangePasswordType;
+use App\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Controller used to manage current user.
+ *
+ * @Route("/profile")
+ * @Security("has_role('ROLE_USER')")
+ *
+ * @author Romain Monteil <monteil.romain@gmail.com>
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/edit", methods={"GET", "POST"}, name="user_edit")
+     */
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'user.updated_successfully');
+
+            return $this->redirectToRoute('user_edit');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/change-password", methods={"GET", "POST"}, name="user_change_password")
+     */
+    public function changePassword(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($encoder->encodePassword($user, $form->get('newPassword')->getData()));
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('security_logout');
+        }
+
+        return $this->render('user/change_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}

tests/Controller/UserControllerTest.php
@@ -1,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Functional test for the controllers defined inside the UserController used
+ * for managing the current logged user.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#functional-tests
+ *
+ * Whenever you test resources protected by a firewall, consider using the
+ * technique explained in:
+ * https://symfony.com/doc/current/cookbook/testing/http_authentication.html
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class UserControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider getUrlsForAnonymousUsers
+     */
+    public function testAccessDeniedForAnonymousUsers(string $httpMethod, string $url)
+    {
+        $client = static::createClient();
+        $client->request($httpMethod, $url);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            'http://localhost/en/login',
+            $response->getTargetUrl(),
+            sprintf('The %s secure URL redirects to the login form.', $url)
+        );
+    }
+
+    public function getUrlsForAnonymousUsers()
+    {
+        yield ['GET', '/en/profile/edit'];
+        yield ['GET', '/en/profile/change-password'];
+    }
+
+    public function testEditUser()
+    {
+        $newUserEmail = 'admin_jane@symfony.com';
+
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+        $crawler = $client->request('GET', '/en/profile/edit');
+        $form = $crawler->selectButton('Save changes')->form([
+            'user[email]' => $newUserEmail,
+        ]);
+        $client->submit($form);
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        /** @var User $user */
+        $user = $client->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy([
+            'email' => $newUserEmail,
+        ]);
+        $this->assertNotNull($user);
+        $this->assertSame($newUserEmail, $user->getEmail());
+    }
+
+    public function testChangePassword()
+    {
+        $newUserPassword = 'new-password';
+
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+        $crawler = $client->request('GET', '/en/profile/change-password');
+        $form = $crawler->selectButton('Save changes')->form([
+            'change_password[currentPassword]' => 'kitten',
+            'change_password[newPassword][first]' => $newUserPassword,
+            'change_password[newPassword][second]' => $newUserPassword,
+        ]);
+        $client->submit($form);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            '/en/logout',
+            $response->getTargetUrl(),
+            'Changing password logout the user.'
+        );
+    }
+}